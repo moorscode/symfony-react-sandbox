540774f9ad50c4c499545c78e3f85ba3cd65cd5c
Jip <jhp.moors@gmail.com>
2023-09-01 22:12:14 +0200

    Remove import

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
